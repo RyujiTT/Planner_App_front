65ddd55740e613663543d57d5a2541cada0eb3a2
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-22 11:48:39 +0900

    [Add]Vuetify Custom CSS

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,7 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["~/assets/sass/main.scss"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ["plugins/axios"],
@@ -50,6 +50,10 @@ export default {
   build: {},
 
   vuetify: {
+    // 開発環境でcustomVariablesを有効にするフラグ
+    // Doc: https://vuetifyjs.com/ja/customization/a-la-carte/
+    treeShake: true,
+    customVariables: ["~/assets/sass/variables.scss"],
     theme: {
       themes: {
         light: {
