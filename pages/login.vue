d9ef5399b874d2e902af75f092fdd6e070c03994
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-08-25 09:58:05 +0900

    [Finish]BeforeLoginLayouts

--- pages/login.vue
+++ pages/login.vue
@@ -1,9 +1,54 @@
 <template>
-  <div>login.vue</div>
+  <user-form-card>
+    <template #user-form-card-content>
+      <v-form ref="form" v-model="isValid">
+        <user-form-email :email.sync="params.user.email" />
+
+        <user-form-password :password.sync="params.user.password" />
+        <v-card-actions>
+          <nuxt-link to="#" class="body-2 text-decoration-none">
+            パスワードを忘れた?
+          </nuxt-link>
+        </v-card-actions>
+        <v-card-text class="px-0">
+          <v-btn
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="myblue"
+            class="white--text"
+            @click="login"
+          >
+            ログインする
+          </v-btn>
+        </v-card-text>
+      </v-form>
+    </template>
+  </user-form-card>
 </template>
 
 <script>
 export default {
   layout: "before-login",
+  data() {
+    return {
+      isValid: false,
+      loading: false,
+      params: { user: { email: "", password: "" } },
+    };
+  },
+  methods: {
+    login() {
+      this.loading = true;
+      setTimeout(() => {
+        this.formReset();
+        this.loading = false;
+      }, 1500);
+    },
+    formReset() {
+      this.$refs.form.reset();
+      this.params = { user: { email: "", password: "" } };
+    },
+  },
 };
 </script>
--- pages/signup.vue
+++ pages/signup.vue
@@ -1,17 +1,25 @@
 <template>
   <user-form-card>
     <template #user-form-card-content>
-      <v-form v-model="isValid">
+      <v-form ref="form" v-model="isValid">
         <user-form-name :name.sync="params.user.name" />
 
         <user-form-email :email.sync="params.user.email" placeholder />
 
         <user-form-password :password.sync="params.user.password" />
-        <v-btn :disabled="!isValid" block color="myblue" class="white--text">
-          登録する
-        </v-btn>
+        <v-card-text>
+          <v-btn
+            :disabled="!isValid || loading"
+            :loading="loading"
+            block
+            color="myblue"
+            class="white--text"
+            @click="signup"
+          >
+            登録する
+          </v-btn>
+        </v-card-text>
       </v-form>
-      {{ params }}
     </template>
   </user-form-card>
 </template>
@@ -22,8 +30,22 @@ export default {
   data() {
     return {
       isValid: false,
+      loading: false,
       params: { user: { name: "", email: "", password: "" } },
     };
+  },
+  methods: {
+    signup() {
+      this.loading = true;
+      setTimeout(() => {
+        this.formReset();
+        this.loading = false;
+      }, 1500);
+    },
+    formReset() {
+      this.$refs.form.reset();
+      this.params = { user: { name: "", email: "", password: "" } };
+    },
   },
 };
 </script>
