a851dd6ea57bb00b5d5cb288a7fc7413c78dc817
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-07 15:30:18 +0900

    [Add]Saved Auth Response in Vuex

--- store/index.js
+++ store/index.js
@@ -24,6 +24,14 @@ export const state = () => ({
       { id: 5, name: "MyProject05", updatedAt: "2020-04-01T12:00:00+09:00" },
     ],
   },
+  user: {
+    current: null,
+  },
+  auth: {
+    token: null,
+    expires: 0,
+    payload: {},
+  },
 });
 
 // 算出プロパティ
@@ -34,6 +42,18 @@ export const mutations = {
   setCurrentProject(state, payload) {
     state.project.current = payload;
   },
+  setCurrentUser(state, payload) {
+    state.user.current = payload;
+  },
+  setAuthToken(state, payload) {
+    state.auth.token = payload;
+  },
+  setAuthExpires(state, payload) {
+    state.auth.expires = payload;
+  },
+  setAuthPayload(state, payload) {
+    state.auth.payload = payload;
+  },
 };
 
 // メソッド
@@ -43,5 +63,19 @@ export const actions = {
     const currentProject =
       state.project.list.find((project) => project.id === id) || null;
     commit("setCurrentProject", currentProject);
+  },
+  getCurrentUser({ commit }, user) {
+    commit("setCurrentUser", user);
+  },
+  getAuthToken({ commit }, token) {
+    commit("setAuthToken", token);
+  },
+  getAuthExpires({ commit }, expires) {
+    expires = expires || 0;
+    commit("setAuthExpires", expires);
+  },
+  getAuthPayload({ commit }, jwtPayload) {
+    jwtPayload = jwtPayload || {};
+    commit("setAuthPayload", jwtPayload);
   },
 };
