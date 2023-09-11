2aedf95f28e465e9c10868446e17162c9d753b84
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-11 12:40:16 +0900

    [Add]Logout Process

--- plugins/auth.js
+++ plugins/auth.js
@@ -38,6 +38,26 @@ class Authentication {
   login(response) {
     this.setAuth(response);
   }
+
+  // axiosのレスポンス401を許容する
+  resolveUnauthorized(status) {
+    return (status >= 200 && status < 300) || status === 401;
+  }
+
+  // Vuexの値を初期値に戻す
+  resetVuex() {
+    this.setAuth({ token: null, expires: 0, user: null });
+    this.store.dispatch("getCurrentProject", null);
+    this.store.dispatch("getProjectList", []);
+  }
+
+  // ログアウト業務
+  async logout() {
+    await this.$axios.$delete("/api/v1/auth_token", {
+      validateStatus: this.resolveUnauthorized(status),
+    });
+    this.resetVuex();
+  }
 }
 
 export default ({ store, $axios }, inject) => {
