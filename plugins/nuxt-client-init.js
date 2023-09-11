835ee52bbcda37384537591fad8039ce1258975c
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-11 12:53:07 +0900

    [Add]Plugins for Stay Login

---
+++ plugins/nuxt-client-init.js
@@ -0,0 +1,10 @@
+// front/plugins/nuxt-client-init.js
+export default async ({ $auth, $axios }) => {
+  await $axios
+    .$post(
+      "/api/v1/auth_token/refresh",
+      {},
+      { validateStatus: (status) => $auth.resolveUnauthorized(status) }
+    )
+    .then((response) => $auth.login(response));
+};
