91bff41f0f903a76c3d341dfabc1a183906acbfc
RyujiTT <129817642+RyujiTT@users.noreply.github.com>
2023-09-11 14:18:39 +0900

    [Add]Error Page

--- middleware/get-project-list 2.js
+++
@@ -1,9 +0,0 @@
-// front/middleware/get-project-list.js
-export default async ({ store, $axios }) => {
-  // プロジェクト一覧が存在しない場合
-  if (!store.state.project.list.length) {
-    await $axios
-      .$get("/api/v1/projects")
-      .then((projects) => store.dispatch("getProjectList", projects));
-  }
-};
